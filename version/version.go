4ccbd7236befc82a4096cca3d8c4cf9ce96362d7
jm4rc05 <cursino@ita.br>
2025-10-27 13:29:44 -0300

    format

--- version/version.go
+++ version/version.go
@@ -1,11 +1,12 @@
 package version
 
 // This version-string will be updated by the developer.
-var Major = "0"
-var Minor = "1"
-var Release = "0"
-var Version = Major + "." + Minor + "." + Release
-
+var (
+	Major   = "0"
+	Minor   = "1"
+	Release = "0"
+	Version = Major + "." + Minor + "." + Release
+)
 
 // The 'BuildNumber' variable will be set by the Go linker during compilation.
 // It must be a package-level 'var' of type string.
